e4bc868626510c0abf7a5ec7b01cc5104c6c38ac
DirtyDome89 <domlar89@icloud.com>
2025-02-20 19:06:11 +0100

    Define PORT variable for server configuration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json()); // Middleware für JSON-Daten
