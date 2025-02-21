db5abd583b428c05719943c18e8558fe914c8e88
DirtyDome89 <domlar89@icloud.com>
2025-02-21 17:37:48 +0100

    Update Tailwind CSS config to remove comment in content array

--- paletten-app/tailwind.config.js
+++ paletten-app/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",  // Überprüft auch JS/TS-Dateien im src Ordner
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
