f22bf25c2053d944ef7fd164db9c3754b31212c3
DirtyDome89 <domlar89@icloud.com>
2025-02-21 17:33:40 +0100

    Refactor HTML structure and replace Tailwind CSS config with CommonJS version

---
+++ paletten-app/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",  // Überprüft auch JS/TS-Dateien im src Ordner
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
