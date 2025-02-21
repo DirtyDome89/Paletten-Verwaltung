8669e04dd985656e3c48d24bf2d7048a84b40073
DirtyDome89 <domlar89@icloud.com>
2025-02-21 18:19:16 +0100

    Add API service for palette management using axios

---
+++ paletten-app/src/services/api.js
@@ -0,0 +1,14 @@
+// src/services/api.js
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api';  // Dein Backend-API-Endpunkt
+
+export const getPalettes = async () => {
+  const response = await axios.get(`${API_URL}/palettes`);
+  return response.data;
+};
+
+export const createPalette = async (data) => {
+  const response = await axios.post(`${API_URL}/palettes`, data);
+  return response.data;
+};
