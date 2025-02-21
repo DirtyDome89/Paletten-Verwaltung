887ce41f71b1399aa30febc756aa5c5102c42b8f
DirtyDome89 <domlar89@icloud.com>
2025-02-21 19:24:09 +0100

    Add @tailwindcss/postcss to devDependencies and update PostCSS configuration

--- paletten-app/paletten-app/postcss.config.cjs
+++ paletten-app/paletten-app/postcss.config.cjs
@@ -1,7 +1,8 @@
 // postcss.config.cjs
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 };
